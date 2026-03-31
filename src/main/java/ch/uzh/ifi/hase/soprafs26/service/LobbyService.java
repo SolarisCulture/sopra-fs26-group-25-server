524491a472eef0353a024e078d01b39790112348
Timmy-Ho <101631726+Timmy-Ho@users.noreply.github.com>
2026-03-31 14:29:58 +0200

    Update LobbyService.java with the transactional import Ref #110

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/LobbyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/LobbyService.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import ch.uzh.ifi.hase.soprafs26.constant.LobbyStatus;
