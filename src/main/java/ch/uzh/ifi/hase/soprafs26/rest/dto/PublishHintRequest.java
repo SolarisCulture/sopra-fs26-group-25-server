8ad7b230c7cb5e46b40564b9957a306c4f2c3c8b
timmyho <timmyho2003@gmail.com>
2026-04-14 15:32:22 +0200

    fix: Added spymasterId to PublishHintRequest DTO Ref #145

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/PublishHintRequest.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/PublishHintRequest.java
@@ -1,11 +1,16 @@
 package ch.uzh.ifi.hase.soprafs26.rest.dto;
 
 public class PublishHintRequest {
+    private Long spymasterId;
     private String hint;
     private int count;
 
+    public Long getSpymasterId() { return spymasterId; }
+    public void setSpymasterId(Long spymasterId) { this.spymasterId = spymasterId; }
+
     public String getHint() { return hint; }
     public void setHint(String hint) { this.hint = hint; }
+
     public int getCount() { return count; }
     public void setCount(int count) { this.count = count; }
 }
