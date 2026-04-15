4af3f5cbb4410655bf66d9eff6ad6508de2a534b
timmyho <timmyho2003@gmail.com>
2026-04-15 18:56:03 +0200

    fix: removed things for story to accomodate earlier PR Ref #19

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/GameController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +63,7 @@ public class GameController {
 
     @PostMapping("/api/games/{lobbyCode}/clue")
     @ResponseStatus(HttpStatus.OK)
-    public void publishHint(@PathVariable String lobbyCode, @RequestBody PublishHintRequest request) {
-        gameService.publishHint(lobbyCode, request.getSpymasterId(), request.getHint(), request.getCount());
+    public void publishHint(@PathVariable String lobbyCode, @RequestBody ClueDTO clueDTO, @RequestHeader Long playerId) {
+        turnService.submitClue(lobbyCode, clueDTO, playerId);
     }
 }
